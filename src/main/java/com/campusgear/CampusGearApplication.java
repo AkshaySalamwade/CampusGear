f0721eab25ab140fbeec1e47d6ae517e18606105
AkshaySalamwade <akshaysalamwade@gmail.com>
2025-10-24 14:17:28 +0530

    Backend changes and README update

---
+++ src/main/java/com/campusgear/CampusGearApplication.java
@@ -0,0 +1,13 @@
+package com.campusgear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusGearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusGearApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campusgear/CampusGearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusgear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusGearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
